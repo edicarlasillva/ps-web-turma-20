359253df0b9134aabe57fd79001e77fa83ad1273
Carla Silva <edicarlasillva@gmail.com>
2024-07-04 21:38:53 -0300

    adicionado delete, bcrypt e uuid

--- back-end-1/start-node/src/index.js
+++ back-end-1/start-node/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import bcrypt from 'bcrypt'
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express()
 app.use(express.json())
@@ -13,7 +15,7 @@ const users = [
   {id: 1, name: 'Alice', available: true },
   {id: 2, name: 'Bob', available: false},
   {id: 3, name: 'Carol', available: true},
-  {id: 4, name: 'Paulo', available: true},
+  {id: 4, name: 'Paulo', available: true}
 ]
 
 app.get('/users', (request, response) => {
@@ -35,7 +37,8 @@ app.post('/users', (request, response) => {
   }
 
   const newUser = {
-    id: users.length + 1,
+    // id: users.length + 1,
+    id: uuidv4(),
     name,
     // se available for null ou undefined, vai receber true
     available: available ?? true
@@ -53,21 +56,21 @@ app.put('/users/:id', (request, response) => {
   const {name: updatedUser, available} = request.body
 
   // Encontrar o usuário que será modificado
-  const user = users.find(user => user.id === parseInt(id))
+  const user = users.find(user => user.id === id)
 
   if (!user) {
     return response.status(404).json({message: 'Usuário não encontrado.'})
   }
 
-  // user.name = updatedUser
-  // user.available = available
+  user.name = updatedUser
+  user.available = available
 
-  const updates = request.body
-  Object.keys(updates).forEach(key => {
-    if (key in user) {
-      user[key] = updates[key]
-    }
-  })
+  // const updates = request.body
+  // Object.keys(updates).forEach(key => {
+  //   if (key in user) {
+  //     user[key] = updates[key]
+  //   }
+  // })
 
   response.status(200).json({message: 'Usuário atualizado com sucesso', user})
 })
@@ -84,6 +87,72 @@ app.get('/users/filtered', (request, response) => {
   }
 
   response.status(200).json(filteredUsers)
+})
+
+app.delete('/users/:id', (request, response) => {
+  const { id } = request.params
+
+  const userIndex = users.findIndex(user => user.id === id)
+
+  if (userIndex === -1) {
+    return response.status(404).json({message: 'Usuário não encontrado.'})
+  }
+
+  // const deletedUser = users.splice(userIndex, 1)[0]
+  const [ deletedUser ] = users.splice(userIndex, 1)
+
+  response.status(200).json({message: 'Usuário removido com sucesso', user: deletedUser})
+})
+
+// ADMIN
+const adminUsers = []
+app.post('/signup', async (request, response) => {
+  try {
+    const { username, password } = request.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Se já existe admim cadastrado com esse username
+    const existingUser = adminUsers.find(user => user.username === username)
+
+    if (existingUser) {
+      return response.status(400).json({message: 'Usuário já existe.'})
+    }
+
+    const newUser = {
+      id: uuidv4(),
+      username,
+      password: hashedPassword
+    }
+
+    adminUsers.push(newUser)
+
+    response.status(201).json({message: 'Admin cadastrado com sucesso.', user: newUser})
+  } catch {
+    response.status(500).json({message: 'Erro ao cadastrar admin.'})
+  }
+})
+
+app.post('/login', async (request, response) => {
+  try {
+    const { username, password } = request.body
+
+    const user = adminUsers.find(user => user.username === username)
+
+    if (!user) {
+      return response.status(404).json({message: 'Admin não encontrado.'})
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return response.status(400).json({message: 'Senha incorreta.'})
+    }
+
+    response.status(200).json({message: 'Login de admim realizado com sucesso.'})
+  } catch {
+    response.status(500).json({message: 'Erro ao fazer login.'})
+  }
 })
 
 app.listen(3000, () => {
