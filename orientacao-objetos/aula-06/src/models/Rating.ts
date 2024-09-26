d8a027aa8a76e85469c44981953f1f6cbd9b4aeb
Carla Silva <edicarlasillva@gmail.com>
2024-09-26 20:52:46 -0300

    Adicionado resolução e-commerce

---
+++ orientacao-objetos/aula-06/src/models/Rating.ts
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import { User } from './User'
+
+export class Rating {
+  private readonly _id: string = uuidv4()
+
+  constructor(
+    private _rate: number, 
+    private _user: User
+  ) {}
+
+  public get id(): string {
+    return this._id
+  }
+
+  public get rate(): number {
+    return this._rate
+  }
+
+  public get user(): User {
+    return this._user
+  }
+}
